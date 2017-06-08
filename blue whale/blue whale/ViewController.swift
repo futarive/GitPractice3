20b09dd89f19ecce1cf472c99b8a15a944b985de
王家永 <futariver@wangjiayongdeMacBook-Pro.local>
2017-06-08 22:02:25 +0800

    Added a new project

---
+++ blue whale/blue whale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blue whale
+//
+//  Created by 王家永 on 2017/6/8.
+//  Copyright © 2017年 王家永. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
