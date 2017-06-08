29358a664fcceb2895f115349f2ab6a17037d259
王家永 <futariver@wangjiayongdeMacBook-Pro.local>
2017-06-08 22:19:58 +0800

    take a test

--- blue whale/blue whale/ViewController.swift
+++ blue whale/blue whale/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func testfunction1() {
+       print("test 1")
     }
-
+    func testFunction2() {
+        print ("test 2")
+    }
 
 }
 
