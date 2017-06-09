847cb69deb8fb93b1c6ced198db4968d429c0264
futarive <15280403654@163.com>
2017-06-09 23:28:25 +0800

    create type choice

--- blue whale/Snippets/Snippets/SnippetData.swift
+++ blue whale/Snippets/Snippets/SnippetData.swift
@@ -7,8 +7,19 @@
 //
 
 import Foundation
+
+enum SnippetType:String {
+    case text = "Text"
+    case photo = "Photo"
+}
+
 struct SnippetData{
-    init(){
-        print("new snippet created")
+    
+    let type:SnippetType
+    
+    init (snippetType:SnippetType) {
+        
+        type = snippetType
+        print("\(type.rawValue) snippet created")
     }
 }
