cad1aae9f383c07d076a6b29d33b7ff92e6ef1f3
futarive <15280403654@163.com>
2017-06-10 00:29:42 +0800

    create a UIAlertController

--- blue whale/Snippets/Snippets/ViewController.swift
+++ blue whale/Snippets/Snippets/ViewController.swift
@@ -18,9 +18,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func creatNewSnippet(_ sender: AnyObject) {
-        let newSnippet = SnippetData()
-        data.append(newSnippet)
+        
+        let alert = UIAlertController(title: "Select a snippet type!",message:nil,preferredStyle:.actionSheet)
+        
+        let textAction =  UIAlertAction(title:"Text",style:.default) {
+            (alert:UIAlertAction!)->Void in self.data.append(SnippetData(snippetType:.text))
+        }
+        
+        let photoAction =  UIAlertAction(title:"Photo",style:.default) {
+            (alert:UIAlertAction!)->Void in self.data.append(SnippetData(snippetType:.photo))
+        }
+        
+        let cancelAction = UIAlertAction(title:"Cancel",style:.cancel,handler:nil)
+        alert.addAction(textAction)
+        alert.addAction(photoAction)
+        alert.addAction(cancelAction)
+        present(alert,animated: true,completion: nil)
     }
-
+    
 }
 
